63d8498097781196ef9fa7e92c812061bd57b469
Hermitcrabb <prathambhandari2000@gmail.com>
2025-08-29 18:04:41 +0545

    Initial commit

---
+++ src/portfolio.js
@@ -0,0 +1,361 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "Pratham Bhandari",
+  title: "Hi all, I'm Pratham",
+  subTitle: emoji(
+    "A passionate Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Flutter / Django and some other cool libraries and frameworks."
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1K13UntFPNN0mav7bdduzk9WmpcaYFKxB/view?usp=sharing", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/Hermitcrabb",
+  linkedin: "https://www.linkedin.com/in/pratham-bhandari-36b903351/",
+  gmail: "prathambhandari2000@gmail.com",
+  instagram:"https://www.instagram.com/_bhandari_pratham_/",
+  
+  
+  // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "CRAZY SOFTWARE DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  skills: [
+  emoji("⚡ Building robust backend systems using Python and Django"),
+  emoji("⚡ Designing and training Machine Learning models using Scikit-learn and TensorFlow"),
+  emoji("⚡ REST API development and integration with frontend frameworks"),
+  emoji("⚡ Data analysis and visualization using Pandas, NumPy, and Matplotlib"),
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "sql-database",
+      fontAwesomeClassname: "fas fa-database"
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "fab fa-docker"
+    },
+    {
+      skillName: "Machine Learning",
+      fontAwesomeClassname: "fas fa-brain"
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "Nepal Engineering College",
+      logo: require("./assets/images/NEC-logo.png"),
+      subHeader: "Bachelors of Science in Computer Engineering",
+      duration: "November 2020 - August 2025",
+      desc: "Graduated in Computer Engineering from Nepal Engineering College (NEC).",
+      descBullets: [
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      ]
+    },
+    {
+      schoolName: "United Academy",
+      logo: require("./assets/images/united-academy-logo.png"),
+      subHeader: "+2 Science",
+      duration: " August 2018 - August 2020",
+      desc: "Completed my High school from a very recognized school",
+      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      progressPercentage: "60%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend",
+      progressPercentage: "80%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "80%"
+    }
+  ],
+  displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: false, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Front-End Developer",
+      company: "Quora",
+      companylogo: require("./assets/images/quoraLogo.png"),
+      date: "May 2017 - May 2018",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      role: "Software Engineer Intern",
+      company: "Airbnb",
+      companylogo: require("./assets/images/airbnbLogo.png"),
+      date: "Jan 2015 - Sep 2015",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "Some of my projects that I have worked on",
+  projects: [
+    {
+      image: require("./assets/images/Meroswastha.png"),
+      projectName: "Mero Swastha",
+      projectDesc: "Mero Swastha is a Flutter-based health and fitness app powered by Firebase, designed to help users maintain a healthier lifestyle. It provides personalized diet and workout recommendations through custom algorithms, allows users to track their profile details such as age, weight, height, gender, and meal type, and offers premium services where users can hire trainers, chat with them in real time, and make secure payments via eSewa.",
+      footerLink: [
+       {
+        name: "",
+        url: ""
+       }
+        //  you can add extra buttons here.
+      ]
+    },
+    {
+      image: require("./assets/images/sportscart.webp"),
+      projectName: "Sports Cart",
+      projectDesc: "Sportscart is an e-commerce web application built with Python and Django, designed for selling sports items online. It features a user-friendly product catalog, shopping cart, and order management system. The platform supports user authentication, secure checkout, and admin tools for managing inventory, categories, and customer orders, making it a complete solution for sports equipment sales.",
+      footerLink: [
+        {
+          name: "",
+          url: ""
+        }
+      ]
+    }
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Machine Learning Using Python",
+      subtitle:
+        "Completed certification in Machine Learning using Python with hands-on projects and practical applications",
+      image: require("./assets/images/simplilearn-logo.png"),
+      imageAlt: "Simplilearn Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://drive.google.com/file/d/1a7cRYIOvO2FyHaCHEY1KrysLxSuSIo_X/view?usp=drive_link"
+        },
+      ]
+    },
+    {
+      title: "Karmalaya X",
+      subtitle:
+        "Participated in Karmalaya X, a college innovation event, where we pitched an AI solution for the Deaf and Hard of Hearing.",
+      image: require("./assets/images/carrd-logo.jpg"),
+      imageAlt: "Carrd Logo",
+      footerLink: [
+        {
+          name: "Karmalaya X",
+          url: "https://drive.google.com/file/d/1_s05R5d0JyW83-a4LWZK94GGGauPNwaU/view?usp=drive_link"
+        }
+      ]
+    },
+
+    /*{
+      title: "PWA Web App Developer",
+      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
+      image: require("./assets/images/pwaLogo.webp"),
+      imageAlt: "PWA Logo",
+      footerLink: [
+        {name: "Certification", url: ""},
+        {
+          name: "Final Project",
+          url: "https://pakistan-olx-1.firebaseapp.com/"
+        }
+      ]
+    } */
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "#",
+  displayMediumBlogs: "false", // Set true to display fetched medium blogs instead of hardcoded ones
+  blogs: [
+    {
+      url: "#",
+      title: "#",
+      description:
+        "#"
+    },
+    {
+      url: "#",
+      title: "#",
+      description:
+        "#"
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "",
+      subtitle: "",
+      slides_url: "",
+      event_url: ""
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Resume Section
+const resumeSection = {
+  title: "Resume",
+  subtitle: "Feel free to download my resume",
+
+  // Please Provide with Your Podcast embeded Link
+  display: true // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:"Discuss a project or just want to say hi? My Inbox is open for all.",
+  number: "+9779861399497",
+  email_address: "prathambhandari2000@gmail.com"
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "twitter", //Replace "twitter" with your twitter username without @
+  display: false // Set true to display this section, defaults to false
+};
+
+const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  splashScreen,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails,
+  isHireable,
+  resumeSection
+};
